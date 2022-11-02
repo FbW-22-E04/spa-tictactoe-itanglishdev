0ed990763e6a91c62c12665fb725ba83de290570
Chris <chrishethertonweb@gmail.com>
2022-11-02 14:28:48 +0100

    created Square and Board components

---
+++ src/components/Board.js
@@ -0,0 +1,29 @@
+import Square from './Square'
+import {Fragment} from 'react'
+
+function Board() {
+    const renderSquare = (i) => {
+      return <Square />;
+    };
+    const status = "Next player: X";
+  
+    return (
+      <Fragment>
+        <div className="status h2 text-center">{status}</div>
+        <div className="board">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </Fragment>
+    );
+  }
+
+  export default Square
+  
--- src/index.js
+++ src/index.js
@@ -6,33 +6,8 @@ import "./styles/index.css";
 // ==========================
 // Components
 // ==========================
-function Square() {
-  return <button className="square">{/* TODO */}</button>;
-}
 
-function Board() {
-  const renderSquare = (i) => {
-    return <Square />;
-  };
-  const status = "Next player: X";
 
-  return (
-    <React.Fragment>
-      <div className="status h2 text-center">{status}</div>
-      <div className="board">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </React.Fragment>
-  );
-}
 
 function Game() {
   return (
